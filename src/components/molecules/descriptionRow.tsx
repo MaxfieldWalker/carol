71e10065927e234463d6cf03d7e2c1d10c246724
Maxfield Walker <maxfield.walker@gmail.com>
2018-09-14 11:30:50 +0900

    Update

--- src/components/molecules/descriptionRow.tsx
+++ src/components/molecules/descriptionRow.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const Row = styled.div`
   padding: 0 9px;
   padding-bottom: 12px;
+  max-height: 180px;
+  overflow-y: scroll;
 `;
 
 export const DescriptionRow: React.StatelessComponent = props => {
